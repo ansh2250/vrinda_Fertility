54035d0e9c1716a3c788c934b5b9419771d6599d
ansh2250 <anshsinha947@gmail.com>
2024-11-09 12:16:49 +0530

    commited all the vrinda fertility website which is created locally

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+//[Custom Javascript]
+
+//Project:	Pregnancy Care - Onepage Html Responsive Template
+//Version:	1.1
+//Primary use:	Pregnancy Care - Onepage Html Responsive Template 
+
+//add your script here
